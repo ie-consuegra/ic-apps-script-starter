ab2fc7bb2c9ba78ddc95bdf248cd951858142fb1
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-03 00:56:48 -0500

    Fix issue when "bundling"

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 printf "\n"
+echo "*  Removing old files..."
+rm dist/backend.js
+rm dist/frontend.html
+rm src/client/bundles/bundle.js
+rm src/client/bundles/bundle.css
+echo "✔️  Done, file can be found as backend.js in dist directory"
+printf "\n"
 echo "*  Bundling backend JavaScript..."
-ls -1 src/server/*.js | sort | while read fn ; do cat "$fn" > dist/backend.js; done
+ls -1 src/server/*.js | sort | while read fn ; do cat "$fn" >> dist/backend.js; done
 echo "✔️  Done, file can be found as backend.js in dist directory"
 printf "\n"
 echo "*  Bundling frontend files..."
 echo "*  JavaScript..."
-ls -1 src/client/js/*.js | sort | while read fn ; do cat "$fn" > src/client/bundles/bundle.js; done
+ls -1 src/client/js/*.js | sort | while read fn ; do cat "$fn" >> src/client/bundles/bundle.js; done
 echo "✔️  Done, file can be found as bundle.js in src/client/bundles"
 echo "*  CSS..."
-ls -1 src/client/css/*.css | sort | while read fn ; do cat "$fn" > src/client/bundles/bundle.css; done
+ls -1 src/client/css/*.css | sort | while read fn ; do cat "$fn" >> src/client/bundles/bundle.css; done
 echo "✔️  Done, file can be found as bundle.js in src/client/bundles"
 echo "*  Building frontend.html file..."
 npx html-build -c html-build-config.js src/client/index.html dist/frontend.html
