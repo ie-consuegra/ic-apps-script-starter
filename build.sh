817b34ef2e1aadeb0b8ea29e23019246d7bc38e7
Ivan Consuegra <ie.consuegra@gmail.com>
2021-09-04 10:47:26 -0500

    Cut two unnecesary lines from build.sh
    Line 20 in build.sh automatically overwrites frontend.html.
    The other two attempted to do that, what was unnecesary.

--- build.sh
+++ build.sh
@@ -18,8 +18,6 @@ ls -1 src/client/css/*.css | sort | while read fn ; do cat "$fn" >> src/client/b
 echo "✔️  Done, file can be found as bundle.js in src/client/bundles"
 echo "*  Building frontend.html file..."
 npx html-build -c html-build-config.js src/client/index.html dist/temp-frontend.html
-cat dist/temp-frontend.html > dist/frontend.html
-rm dist/temp-frontend.html
 echo "✔️  Done, file can be found in dist directory"
 printf "\n"
 echo "👍  Ready to test or deploy"
